dc62f7de51d08a02486291219c137f39cf9e7c85
Kalle Sirkesalo <kalle.sirkesalo@eficode.com>
2018-07-30 13:37:02 +0300

    Added first unit tests, still working on reporting.

---
+++ TodoTests/BasicTests.cs
@@ -0,0 +1,27 @@
+using Xunit;
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+public class BasicTests
+    : IClassFixture<WebApplicationFactory<RazorPagesProject.Startup>>
+{
+    private readonly HttpClient _client;
+
+    public BasicTests(WebApplicationFactory<RazorPagesProject.Startup> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetHomePage()
+    {
+        // Act
+        var response = await _client.GetAsync("/");
+
+        // Assert
+        response.EnsureSuccessStatusCode(); // Status Code 200-299
+        Assert.Equal("text/html; charset=utf-8",
+            response.Content.Headers.ContentType.ToString());
+    }
+}
